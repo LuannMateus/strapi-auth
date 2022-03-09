57f5f4402c61660adb47444cb1bd8ae80f6b6b6f
LuannMateus <luanmateuslima34@gmail.com>
2022-03-09 09:45:08 -0300

    Add initial project and controllers of create and find

---
+++ api/post/controllers/post.js
@@ -0,0 +1,32 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+   async find(ctx) {
+    let entities;
+
+    const query = { ...ctx.query, user: ctx.state.user.id };
+
+    if (ctx.query._q) {
+      entities = await strapi.services.post.search(query);
+    } else {
+      entities = await strapi.services.post.find(query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.post }));
+  },
+
+  async create(ctx) {
+    const { id } = ctx.state.user;
+    const { title, content } = ctx.request.body;
+    const post = { title, content, user: id };
+
+    const entity = await strapi.services.post.create(post);
+
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1cfd2ecb-6f29-4921-9e9e-a146b288dae5'
+};
