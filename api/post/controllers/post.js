c003bb279bd6cb9757130c098af5958616750ad7
LuannMateus <luanmateuslima34@gmail.com>
2022-03-09 10:31:35 -0300

    Add update and delete controllers

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -60,5 +60,47 @@ module.exports = {
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
 
+  async update(ctx) {
+    const { id } = ctx.params;
 
+    let entity;
+
+    const post = await strapi.services.post.find({
+      id,
+      user: ctx.state.user.id
+    });
+
+    if (!post || !post.length) {
+      return ctx.unauthorized('You cannot update this post.');
+    }
+
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.update({ id },
+        {...data, user: ctx.state.user.id}, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.post.update({ id },
+        {...ctx.request.body, user: ctx.state.user.id});
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const post = await strapi.services.post.find({
+      id,
+      user: ctx.state.user.id
+    });
+
+    if (!post || !post.length) {
+      return ctx.unauthorized('You cannot delete this post.');
+    }
+
+    const entity = await strapi.services.post.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
 };
